c894ffc27b8c9c8da0213f47de1ff5c16282dabd
Igor Matheus <igor_memories@hotmail.com>
2017-04-01 21:12:59 -0400

    1.003
    -Controler Sabor criado
    - Atualização no Banco de Dados

---
+++ PizzaExpress/PizzaExpress/Controllers/SaborController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PizzaExpress.Controllers
+{
+    public class SaborController : Controller
+    {
+        // GET: Sabor
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
