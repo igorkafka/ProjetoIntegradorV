b79a5a712efa099d487ddf9e54a73baf224f54bb
Igor Matheus <igor_memories@hotmail.com>
2017-08-12 15:44:14 -0400

    - Remoção de bibliotecas que não serão mais utilizadas
    - Remoção do Jquery.Unobustrive

--- PizzaExpress/PizzaExpress/App_Start/BundleConfig.cs
+++ PizzaExpress/PizzaExpress/App_Start/BundleConfig.cs
@@ -9,11 +9,12 @@ namespace PizzaExpress
         public static void RegisterBundles(BundleCollection bundles)
         {
            ;
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js", "~/Scripts/jquery-ui-1.12.1.js", "~/Scripts/jquery-ui-1.12.1.min.js", "~/Scripts/jquery.datetimepicker.full.js"));
+                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*", "~/Scripts/jquery.unobtrusive-ajax.js", "~/Scripts/jQueryFixes.js"));
+                        "~/Scripts/jquery.validate*", "~/Scripts/jQueryFixes.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
